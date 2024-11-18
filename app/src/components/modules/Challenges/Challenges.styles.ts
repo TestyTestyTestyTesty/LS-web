f1a57df0e4dfd392c182f4f99605feaef74e6d37
Tomasz Sikora <sikoratomaszit@gmail.com>
2024-11-18 12:08:10 +0100

    change challenges

--- app/src/components/modules/Challenges/Challenges.styles.ts
+++ app/src/components/modules/Challenges/Challenges.styles.ts
@@ -1,9 +1,15 @@
 import { breakpointsEnum } from "@lib"
 import { Box, styled } from "@mui/material"
-
 export const ChallengesWrapper = styled(Box, {
 	name: "ChallengesWrapper",
 })(({ theme }) => ({
+	background: theme.palette.lsGrey.g25,
+}))
+export const ChallengesInsideWrapper = styled(Box, {
+	name: "ChallengesInsideWrapper",
+})(({ theme }) => ({
+	padding: "2rem 0 0 0",
+	backgroundColor: theme.palette.lsGrey.g25,
 	display: "flex",
 	flexDirection: "column",
 	gap: "3rem",
@@ -11,7 +17,6 @@ export const ChallengesWrapper = styled(Box, {
 		gap: "2rem",
 	},
 }))
-
 export const ChallengesContainer = styled(Box, {
 	name: "ChallengesContainer",
 })({
